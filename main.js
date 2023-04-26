bcb8cafc7072a85877781ac4658a6fffc7921ab9
Faheema-Intkhab <108613150+Faheema-Intkhab@users.noreply.github.com>
2023-04-26 13:58:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        RightWristX = results[0].pose.rightWrist.x;
+        difference = floor(LeftWristX - RightWristX);
+    }
+
+    
+}
+
+function draw()
+{
+    background('#969A97');
+    textSize(difference);
+    fill('#FFE787')
+    text('Faheema',50,400)
+}
+
